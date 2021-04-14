d5c977c33711cbf21fc29203d10fa31f00aadc67
Irina Shapovalova <irishapovalova13@gmail.com>
2021-04-14 13:12:43 -0700

    DB config through env

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,14 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :fyp, Fyp.Repo,
   # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  username: {:system, "REPO_USERNAME"},
+  password: {:system, "REPO_PASSWORD"},
+  database: {:system, "REPO_DATABASE"},
+  hostname: {:system, "REPO_HOSTNAME"},
+  port: {:system, :integer, "ETSP_PORT"},
+  pool_size: {:system, :integer, "POOL_SIZE", 10}
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
