e1b64944de7b512a06792ad668a885c7a9005a4c
Irina Shapovalova <irishapovalova13@gmail.com>
2021-05-27 07:05:52 -0700

    progress

--- lib/fyp/scraping/shelter_poteryashka.ex
+++ lib/fyp/scraping/shelter_poteryashka.ex
@@ -1,10 +1,11 @@
 defmodule Fyp.Scraping.ShelterPoteryashka do
   require Logger
 
-  def get_pets_url(url) do
-    Logger.warn("Getting data from #{url}")
+  def get_pets_url(page_url, base_url) do
+    full_url = base_url <> page_url
+    Logger.warn("Getting data from #{full_url}")
 
-    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.get(url),
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.get(full_url),
          {:ok, html_tree} <- Floki.parse_document(body) do
       urls =
         html_tree
@@ -17,26 +18,24 @@ defmodule Fyp.Scraping.ShelterPoteryashka do
       have_next =
         html_tree
         |> Floki.find(".pagesBlockuz2")
-        |> Floki.find("a+b")
-        |> IO.inspect
+        |> Floki.find("b+a")
         |> Floki.attribute("href")
 
-      urls
-#      case have_next do
-#        [] -> urls
-#        [next_page] -> urls ++ get_pets_url(next_page)
-#      end
+      case have_next do
+        [] -> urls
+        [next_page] -> urls ++ get_pets_url(next_page, base_url)
+      end
     else
       {:error, %HTTPoison.Response{status_code: 404}} ->
-        Logger.error("HTTPoison. Page is not found. Url: #{url}")
+        Logger.error("HTTPoison. Page is not found. Url: #{full_url}")
         []
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.error("HTTPoison. Page error. Url: #{url}. Reason: #{inspect(reason)}")
+        Logger.error("HTTPoison. Page error. Url: #{full_url}. Reason: #{inspect(reason)}")
         []
 
       {:error, reason} ->
-        Logger.error("Error. Url: #{url}. Reason: #{inspect(reason)}")
+        Logger.error("Error. Url: #{full_url}. Reason: #{inspect(reason)}")
         []
     end
   end
@@ -61,13 +60,14 @@ defmodule Fyp.Scraping.ShelterPoteryashka do
     |> Floki.text()
   end
 
-  def get_pet_photos(body) do
+  def get_pet_photos(body, base_url) do
     body
     |> Floki.find("td.eText")
     |> Floki.find("tr")
     |> Floki.find("td:first-child")
     |> Floki.find("img")
     |> Floki.attribute("src")
+    |> Enum.map(fn photo_url -> base_url <> photo_url end)
   end
 
   def get_pet_description(body) do
@@ -75,7 +75,8 @@ defmodule Fyp.Scraping.ShelterPoteryashka do
     |> Floki.find("td.eText")
     |> Floki.find("tr")
     |> Floki.find("td:last-child")
-    |> Floki.text(sep: "\n", deep: false)
+    |> IO.inspect
+    |> Floki.text(deep: false)
     |> String.trim_leading()
     |> String.trim_trailing()
     |> String.replace("\u00A0", "")
@@ -88,23 +89,23 @@ defimpl Fyp.Scraping.Shelters, for: ShelterPoteryashka do
 
   def get_pets(%ShelterPoteryashka{link: base_url}) do
     categories = [
-      {"http://poteryashka.spb.ru/load/1", 2}, # Dogs
-      {"http://poteryashka.spb.ru/load/3", 1}  # Cats
+      {"/load/1", 2}, # Dogs
+      {"/load/3", 1}  # Cats
     ]
 
     _pets =
       Enum.each(categories, fn {category_url, type_id} ->
-        get_pets_url(category_url)
+        get_pets_url(category_url, base_url)
         |> get_pets_html_body(base_url)
         |> Enum.map(fn body ->
           %{
             name: get_pet_name(body),
             breed: nil,
             gender: nil,
-            birth: get_pet_birthday(body),
+            birth: nil,
             height: nil,
             description: get_pet_description(body),
-            photos: get_pet_photos(body),
+            photos: get_pet_photos(body, base_url),
             shelter_id: 4,
             type_id: type_id
           }
--- test/fyp_web/aggregation/shelter_poteryaska_test.exs
+++ test/fyp_web/aggregation/shelter_poteryaska_test.exs
@@ -3,6 +3,38 @@ defmodule ShelterPoteryaskaTest do
   import Fyp.Scraping.ShelterPoteryashka
 
   test "Correct parsing" do
-    Fyp.Scraping.Shelters.get_pets(%ShelterPoteryashka{link: "http://poteryashka.spb.ru/"})
+    base_url = "http://poteryashka.spb.ru"
+    pet =
+      get_pets_html_body(["/load/1-1-0-1754"], base_url)
+      |> Enum.map(fn body ->
+        %{
+          name: get_pet_name(body),
+          breed: nil,
+          gender: nil,
+          birth: nil,
+          height: nil,
+          description: get_pet_description(body),
+          photos: get_pet_photos(body, base_url),
+          shelter_id: 4,
+          type_id: 1
+        }
+      end)
+      |> hd
+
+    assert %{
+             name: "Щенки \"Лунные\"",
+             breed: nil,
+             gender: nil,
+             birth: nil,
+             height: nil,
+             description:
+               "Два чудесных щеночка, им всего по 2,5 месяца. Детишки домашние, здоровенькие, упитанные и здоровые.\nЗвоните!",
+             photos: _,
+             shelter_id: 4,
+             type_id: 1
+           } = pet
+
+    %{photos: photos} = pet
+    assert length(photos) == 1
   end
 end
